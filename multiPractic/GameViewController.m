a7378b5622fb58c117854fffca71c97b2f1b24f4
aorzes <anton.orzes@gmail.com>
2016-01-16 19:22:13 +0100

    pokretanje game center

--- multiPractic/GameViewController.m
+++ multiPractic/GameViewController.m
@@ -25,21 +25,22 @@
 
 -(void)playerAuthenticated{
 
-   // [[gameCenterFiles sharedGameKitHelper]findMatchWithMinPlayers:2 maxPlayers:2 viewControllelr:self delegate:self];
     [[gameCenterFiles sharedInstance]findMatchWithMinPlayers:2 maxPlayers:2 viewControllelr:self delegate:self];
+    NSLog(@"findMatchWithMinPlayers");
 }
 -(void)dealloc{
     [[NSNotificationCenter defaultCenter]removeObserver:self];
 
 }
 -(void)match:(GKMatch *)match didReceiveData:(NSData *)data fromRemotePlayer:(GKPlayer *)player{
-
+    NSLog(@"Received data");
 }
 -(void)matchStarted{
+    NSLog(@"Match started");
 
 }
 -(void)mathcEnded{
-
+    NSLog(@"Match ended");
 }
 
 - (void)viewDidLoad
